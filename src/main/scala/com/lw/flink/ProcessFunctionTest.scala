e212ddc0109ff9e79e7dc5e019d34ba50f1be8d5
Virgil <wyluowen@163.com>
2020-08-19 11:01:14 +0800

    状态编程

--- src/main/scala/com/lw/flink/ProcessFunctionTest.scala
+++ src/main/scala/com/lw/flink/ProcessFunctionTest.scala
@@ -1,7 +1,9 @@
 package com.lw.flink
 
+import org.apache.flink.api.common.functions.RichFlatMapFunction
 import org.apache.flink.api.common.state.{ValueState, ValueStateDescriptor}
 import org.apache.flink.api.scala.typeutils.Types
+import org.apache.flink.configuration.Configuration
 import org.apache.flink.streaming.api.TimeCharacteristic
 import org.apache.flink.streaming.api.functions.KeyedProcessFunction
 import org.apache.flink.streaming.api.functions.timestamps.BoundedOutOfOrdernessTimestampExtractor
@@ -9,10 +11,7 @@ import org.apache.flink.streaming.api.scala._
 import org.apache.flink.streaming.api.windowing.time.Time
 import org.apache.flink.util.Collector
 
-/**
- * 需求：监控温度传感器的温度值，如果温度值在5秒钟之内(processing time)连
- * 续上升，则报警
- */
+
 object ProcessFunctionTest {
 
   def main(args: Array[String]): Unit = {
@@ -39,8 +38,26 @@ object ProcessFunctionTest {
     val processedStream = dataStream.keyBy(_.id)
       .process(new TempIncreAlert())
 
+    val processedStream2 = dataStream.keyBy(_.id)
+      //      .process(new TempChangeAlert(10.0))
+      .flatMap(new TempChangeAlert2(10.0))
+
+    val processedStream3 = dataStream.keyBy(_.id)
+      .flatMapWithState[(String, Double, Double), Double] {
+        // 如果没有状态，也就是没有数据来过，那么就将当前数据温度值存入状态
+        case (input: SensorReading, None) => (List.empty, Some(input.temperature))
+        // 如果有状态就和上一次温度比较
+        case (input: SensorReading, lastTemp: Some[Double]) =>
+          val diff = (input.temperature - lastTemp.get).abs
+          if (diff >= 10) {
+            (List((input.id, lastTemp.get, input.temperature)), Some(input.temperature))
+          } else
+            (List.empty, Some(input.temperature))
+
+      }
+
     dataStream.print("data stream ")
-    processedStream.print("process stream")
+    processedStream2.print("process stream")
 
 
     env.execute("ProcessFunction test")
@@ -49,13 +66,47 @@ object ProcessFunctionTest {
 
 }
 
+// 温度值变化超过10度就报警
+class TempChangeAlert(threshold: Double) extends KeyedProcessFunction[String, SensorReading, (String, Double, Double)] {
+  lazy val lastTempState: ValueState[Double] = getRuntimeContext.getState(new ValueStateDescriptor[Double]("lastTemp", Types.of[Double]))
+
+  override def processElement(value: SensorReading, ctx: KeyedProcessFunction[String, SensorReading, (String, Double, Double)]#Context, out: Collector[(String, Double, Double)]): Unit = {
+    val lastTemp = lastTempState.value()
+    // 当前温度和上一次温度差值大于阀值则输出报警信息
+    if ((value.temperature - lastTemp).abs >= threshold) {
+      out.collect((value.id, lastTemp, value.temperature))
+    }
+    //更新上一次温度状态
+    lastTempState.update(value.temperature)
+
+  }
+
+}
+// 使用富函数类，使map这类带状态
+class TempChangeAlert2(threshold: Double) extends RichFlatMapFunction[SensorReading, (String, Double, Double)] {
+  var lastTempState: ValueState[Double] = _
+
+  override def open(parameters: Configuration): Unit = {
+    lastTempState = getRuntimeContext.getState(new ValueStateDescriptor[Double]("lastTemp", Types.of[Double]))
+  }
+
+  override def flatMap(value: SensorReading, out: Collector[(String, Double, Double)]): Unit = {
+    val lastTemp = lastTempState.value()
+    // 当前温度和上一次温度差值大于阀值则输出报警信息
+    if ((value.temperature - lastTemp).abs >= threshold) {
+      out.collect((value.id, lastTemp, value.temperature))
+    }
+    //更新上一次温度状态
+    lastTempState.update(value.temperature)
+  }
+}
+
+// 监控温度传感器的温度值，如果温度值在5秒钟之内(processing time)连续上升，则报警
 class TempIncreAlert extends KeyedProcessFunction[String, SensorReading, String] {
   // 定义一个状态，用来保存上一个传感器的温度值
   lazy val lastTemp: ValueState[Double] = getRuntimeContext.getState(new ValueStateDescriptor[Double]("lastTemp", Types.of[Double]))
-
   // 保存注册的定时器的时间戳
   lazy val currentTimer: ValueState[Long] = getRuntimeContext.getState(new ValueStateDescriptor[Long]("timer", Types.of[Long]))
-
 
   override def processElement(i: SensorReading, context: KeyedProcessFunction[String, SensorReading, String]#Context, collector: Collector[String]): Unit = {
 
@@ -67,13 +118,13 @@ class TempIncreAlert extends KeyedProcessFunction[String, SensorReading, String]
     lastTemp.update(i.temperature)
 
     val curTimerTs = currentTimer.value()
-    if (i.temperature < prevTemp || prevTemp == 0.0 ) {
+    if (i.temperature < prevTemp || prevTemp == 0.0) {
       // 温度下降或者是第一个温度值，删除定时器
       context.timerService().deleteProcessingTimeTimer(curTimerTs)
       // 清空状态变量
       currentTimer.clear()
     } else if (i.temperature > prevTemp && curTimerTs == 0) {
-      // 温度上升且我们并没有设置定时器，就注册一个定时器，等一秒钟后触发，看这5秒内的数据
+      // 温度上升且我们并没有设置定时器，就注册一个定时器，等5秒钟后触发，看这5秒内的数据
       val timerTs = context.timerService().currentProcessingTime() + 5000
       context.timerService().registerProcessingTimeTimer(timerTs)
       currentTimer.update(timerTs)
@@ -81,12 +132,10 @@ class TempIncreAlert extends KeyedProcessFunction[String, SensorReading, String]
   }
 
   override def onTimer(timestamp: Long, ctx: KeyedProcessFunction[String, SensorReading, String]#OnTimerContext, out: Collector[String]): Unit = {
-//    super.onTimer(timestamp, ctx, out)
-    out.collect("警报警报！"+ctx.getCurrentKey+"温度连续上升！")
+    //    super.onTimer(timestamp, ctx, out)
+    out.collect("警报警报！" + ctx.getCurrentKey + "温度连续上升！")
     currentTimer.clear()
 
   }
-
-
 
 }
