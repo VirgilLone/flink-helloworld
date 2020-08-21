43883810ee2378a623509f63821f90bdcf7737ce
Virgil <wyluowen@163.com>
2020-08-21 17:39:46 +0800

    flink对乱序数据的处理：1。设延迟时间 2。允许迟到数据 3。侧输出流进行后续手动处理

--- src/main/scala/com/lw/flink/Windowtest.scala
+++ src/main/scala/com/lw/flink/Windowtest.scala
@@ -15,6 +15,8 @@ object Windowtest {
 
     // 使用event时间
     env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
+    // 设置周期性插入水位线的时间 flink默认是200ms
+//    env.getConfig.setAutoWatermarkInterval(300L)
 
 
     val txtStream = env.socketTextStream("localhost", 7777)
@@ -24,20 +26,29 @@ object Windowtest {
       SensorReading(dataArrary(0).trim, dataArrary(1).trim.toLong, dataArrary(2).trim.toDouble)
     })
       //      .assignAscendingTimestamps(_.timestamp*1000)
-      // 延迟1s上涨水位
+      // 设置延迟时间 1秒  最好等于最大乱序时间   watermark=当前最大时间戳-延迟时间,
+      // 使得watermark不断后移，直到触发一个窗口关闭的时间
       .assignTimestampsAndWatermarks(new BoundedOutOfOrdernessTimestampExtractor[SensorReading](Time.seconds(1)) {
-        override def extractTimestamp(t: SensorReading) = t.timestamp * 1000
+        override def extractTimestamp(t: SensorReading) = t.timestamp * 1000L
       })
 
 
     val maxTemp = dataStream.map(e => (e.id, e.temperature))
       .keyBy(_._1)
 //      .window(SlidingEventTimeWindows.of(Time.seconds(15),Time.seconds(15),Time.hours(-8)))
-      .timeWindow(Time.seconds(5)) // assigner 决定数据去哪个窗口
-      .reduce((data1, data2) => (data1._1, data1._2.min(data2._2))) // windowFunction 窗口关闭时做的操作
+      // assigner 决定数据去哪个窗口
+      .timeWindow(Time.seconds(5))
+      // 设置窗口允许处理迟到数据为1分钟，迟到数据放入原来本该关闭的窗口再和本窗口已经输出的结果做聚合
+      .allowedLateness(Time.minutes(1))
+      // 设置迟到的数据写入侧输出流
+      .sideOutputLateData(new OutputTag[(String,Double)]("lated"))
+      // windowFunction 窗口关闭时做的操作
+      .reduce((data1, data2) => (data1._1, data1._2.min(data2._2)))
 
     txtStream.print("source-->")
     maxTemp.print("min input")
+
+    maxTemp.getSideOutput(new OutputTag[(String,Double)]("lated")).print("side output data")
 
     env.execute("window test")
 
