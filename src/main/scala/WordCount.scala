dd36bf683a5c974ffa29a938b716d7d7ed50f963
Virgil <wyluowen@163.com>
2020-08-24 21:11:21 +0800

    spark的包会和flink冲突

--- src/main/scala/WordCount.scala
+++ src/main/scala/WordCount.scala
@@ -1,16 +1,16 @@
-import org.apache.spark.SparkContext
-import org.apache.spark.SparkContext._
-import org.apache.spark.SparkConf
-
-object WordCount {
-
-  def main(args: Array[String]) {
-    val inputFile =  "file:///Users/xyj/developer/logs/word2.txt"
-    val conf = new SparkConf().setAppName("WordCount").setMaster("local")
-    val sc = new SparkContext(conf)
-    val textFile = sc.textFile(inputFile,1)
-    val wordCount = textFile.flatMap(line => line.split("\\s+")).map(word => (word, 1)).reduceByKey((a, b) => a + b)
-    wordCount.foreach(println)
-
-  }
-}
+//import org.apache.spark.SparkContext
+//import org.apache.spark.SparkContext._
+//import org.apache.spark.SparkConf
+//
+//object WordCount {
+//
+//  def main(args: Array[String]) {
+//    val inputFile =  "file:///Users/xyj/developer/logs/word2.txt"
+//    val conf = new SparkConf().setAppName("WordCount").setMaster("local")
+//    val sc = new SparkContext(conf)
+//    val textFile = sc.textFile(inputFile,1)
+//    val wordCount = textFile.flatMap(line => line.split("\\s+")).map(word => (word, 1)).reduceByKey((a, b) => a + b)
+//    wordCount.foreach(println)
+//
+//  }
+//}
